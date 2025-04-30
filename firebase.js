7abc47745c2e17ebad3ea841472942c9b1b908fa
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2025-04-30 16:27:37 +0300

    firebase22

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0NCoBEthxyC0Io61--nyUJGojGmeG3YQ",
+    authDomain: "saucia-6c18c.firebaseapp.com",
+    projectId: "saucia-6c18c",
+    storageBucket: "saucia-6c18c.firebasestorage.app",
+    messagingSenderId: "374396762838",
+    appId: "1:374396762838:web:56d626a30628604ca62777",
+    measurementId: "G-26GTMC69QB"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
